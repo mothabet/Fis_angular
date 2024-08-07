8cd62b6d2cbabf47ebacba6201452e60d06dd8ea
mothabet <zizoelbeltagy266@gmail.com>
2024-08-07 15:10:46 +0300

    Message Handle

---
+++ src/app/messages/Dtos/SubCodeHomeDto.ts
@@ -0,0 +1,14 @@
+export interface IAddSubCode {
+    Id:number;
+    QuestionCode:string,
+    arName: string,
+    enName: string,
+}
+export interface ISubCode{
+    Id:number,
+    arName: string,
+    enName: string,
+    QuestionCode:string,
+    codeId :number
+
+}
