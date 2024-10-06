dc56752a561c2353ca39f3a0250936cd0d381726
mothabet <zizoelbeltagy266@gmail.com>
2024-10-06 16:34:36 +0300

    handle table error

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,5 +7,4 @@ export const environment = {
 //     production: true,
 //     apiUrl: 'https://fisapp.azurewebsites.net/api/api/',
 //     dirUrl: 'https://fisapp.azurewebsites.net/api/wwwroot/',
-
 // };
