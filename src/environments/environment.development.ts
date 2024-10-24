d2ccf9b5b5b21ce88ff65260350854052abcb903
seifzahran2 <seif.zahran2200@gmail.com>
2024-10-24 04:37:57 +0300

    connection string

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
-// export const environment = {
-//     production: true,
-//     apiUrl: 'https://localhost:44326/api/',
-//     dirUrl: 'https://localhost:44326/wwwroot/',
-// };
 export const environment = {
     production: true,
-    apiUrl: 'https://fisapp.azurewebsites.net/api/api/',
-    dirUrl: 'https://fisapp.azurewebsites.net/api/wwwroot/',
+    apiUrl: 'https://localhost:44326/api/',
+    dirUrl: 'https://localhost:44326/wwwroot/',
 };
+// export const environment = {
+//     production: true,
+//     apiUrl: 'https://fisapp.azurewebsites.net/api/api/',
+//     dirUrl: 'https://fisapp.azurewebsites.net/api/wwwroot/',
+// };
